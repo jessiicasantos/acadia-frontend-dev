6a9026e9087e240b9e9ac3435536a37d7529cfd9
Jessica Nascimento <jessiicansantos@gmail.com>
2026-02-15 23:40:59 -0300

    Add modal, adjust colors and btns hover

---
+++ js/modal.js
@@ -0,0 +1,11 @@
+const modal = document.getElementById('global-modal');
+
+const toggleModal = (show = true) => {
+    modal.classList.toggle('active', show);
+    overlay.classList.toggle('active', show);
+    if (!show) document.getElementById('product-form').reset();
+};
+
+document.getElementById('add-sample-btn').addEventListener('click', () => toggleModal(true));
+document.getElementById('close-modal').addEventListener('click', () => toggleModal(false));
+overlay.addEventListener('click', () => toggleModal(false));
--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
+const overlay = document.getElementById('global-overlay');
+const closeBtn = document.getElementById('close-cart');
+
 const btnAddCart = document.querySelectorAll('.add-cart');
+
+function openCart() {
+    overlay.classList.add('active');
+}
+
+function closeCart() {
+    overlay.classList.remove('active');
+}
 
 btnAddCart.forEach(btn => {
     btn.addEventListener('click', (event) => {
@@ -6,5 +17,10 @@ btnAddCart.forEach(btn => {
         
         btn.classList.toggle('added');
         btn.textContent = btn.classList.contains('added') ? 'Added' : 'Add';
+
+        openCart();
     });
 });
+
+// closeBtn.addEventListener('click', closeCart);
+overlay.addEventListener('click', closeCart);
